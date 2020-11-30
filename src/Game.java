55d8429f5aa5b374ef57791c6cb3052c4b959884
sergiogutierrez2 <58555535+sergiogutierrez2@users.noreply.github.com>
2020-11-29 19:04:03 -0800

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,14 @@
 public class Game {
 
-	// view
+    private BlockingQueue<Message> queue;
+    private View view; // Direct reference to view
+    private Model model; // Direct reference to model
 	
-	//model
+	public Controller(View view, Model model, BlockingQueue<Message> queue) {
+        this.view = view;
+        this.model = model;
+        this.queue = queue;
+	}
 	
 	public static void main(String[] args) {
 
