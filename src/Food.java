63b7a2267856bc2ab4c474d2a5c2fa6c04e06b85
sergiogutierrez2 <58555535+sergiogutierrez2@users.noreply.github.com>
2020-12-01 01:49:32 -0800

    Update Food.java

--- src/Food.java
+++ src/Food.java
@@ -1,8 +1,16 @@
 public class Food {
 
+     /**
+     * Produces the food that the snake will eat, which
+     * will be in one of the tiles of the board.
+     */
     public Tile location;
     public Board board;
 
+     /**
+     * Produces a food object which will be placed on a tile of the board
+     * @param board the board in which the food will be placed
+     */
     public Food(Board board) {
         this.board = board;
         this.location = null;
@@ -14,6 +22,10 @@ public class Food {
         board.tiles[x][y].setInsideTile("food");
     }
 
+     /**
+     * Spawns food on a random tile that contains
+     * nothing, if food is not on the board
+     */
     public void spawnFood() {
         //needs to look at board and choose unoccupied tile
         if (!foodIsOnBoard()) {
@@ -28,6 +40,10 @@ public class Food {
         }
     }
 
+    /**
+    * Checks whether food is on board
+    * @return true or false, depending if food is found or not on the board.
+    */
     public boolean foodIsOnBoard() {
         //iterate through tiles of board and check if food is there
         for (int i = 0; i < board.tiles.length; i++) {
@@ -39,7 +55,11 @@ public class Food {
         }
         return false;
     }
-
+    
+    /**
+     * Returns the String with the location of the food
+     * @return the String with the location of the food
+     */
     @Override
     public String toString() {
         return location.toString();
