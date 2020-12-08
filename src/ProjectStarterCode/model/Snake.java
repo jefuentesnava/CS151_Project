9e4d3d734fb54b6e068e767838b5985e3d8ef304
sergiogutierrez2 <58555535+sergiogutierrez2@users.noreply.github.com>
2020-12-08 01:42:33 -0800

    Update Snake.java
    Added documentation

--- src/ProjectStarterCode/model/Snake.java
+++ src/ProjectStarterCode/model/Snake.java
@@ -1,17 +1,14 @@
-package ProjectStarterCode.model;
-
 import java.util.LinkedList;
 
 public class Snake {
-
     /**
-    * Represents the snake that will be moving
-    * throughout the board.
-    */
-    private boolean alive;
-    private int size;
-    public Direction direction;
-    public LinkedList<Tile> location;
+     * Represents the snake that will be moving
+     * throughout the board.
+     */
+    private boolean alive; /** it is used to check if the Snake is alive */
+    private int size; /** it is used to check the length of the Snake */
+    public Direction direction; /** This is the direction taken by the Snake */
+    public LinkedList<Tile> location; /** it is used to keep track of the Snake's location*/
 
     // Directions will be up, down, left and right
     enum Direction {
@@ -22,10 +19,10 @@ public class Snake {
     }
 
     /**
-    * Creates a Snake the length of three tiles.
-    * It'll go down by default.
-    * @param board the board in which the Snake will be placed
-    */
+     * Creates a Snake the length of three tiles.
+     * It'll go down by default.
+     * @param board the board in which the Snake will be placed
+     */
     public Snake(Board board) {
         alive = true;
         direction = Direction.DOWN;
@@ -39,7 +36,7 @@ public class Snake {
         size = location.size();
     }
 
-     /**
+    /**
      * Will make the Snake be dead by changing the status of alive
      * @return alive
      */
@@ -49,22 +46,22 @@ public class Snake {
     }
 
     /**
-    * Gets the status on whether the snake is alive or not
-    * @return alive
-    */
+     * Gets the status on whether the snake is alive or not
+     * @return alive
+     */
     public boolean isAlive() {
         return alive;
     }
 
     /**
-    * Gets the size of the Snake
-    * @return size of the Snake
-    */
+     * Gets the size of the Snake
+     * @return size of the Snake
+     */
     public int getSize() {
         return size;
     }
 
-     /**
+    /**
      * Checks if the size of the Snake has exceeded the length of 40 tiles
      * which is the win condition for the game
      * @return whether size is greater than 40
@@ -74,8 +71,8 @@ public class Snake {
     }
 
     /**
-    * Prints the snake
-    */
+     * Prints the snake
+     */
     public void printSnake() {
         for (Tile snakePiece : location) {
             System.out.print(snakePiece);
@@ -84,30 +81,42 @@ public class Snake {
     }
 
     /**
-    * Updates the size of the snake
-    */
+     * Updates the size of the snake
+     */
     public void updateSize() {
         size = location.size();
     }
 
+    /**
+     * Makes the Snake go left
+     */
     public void goLeft() {
         if (!direction.equals(Direction.RIGHT)) {
             direction = Direction.LEFT;
         }
     }
 
+    /**
+     * Makes the Snake go Right
+     */
     public void goRight() {
         if (!direction.equals(Direction.LEFT)) {
             direction = Direction.RIGHT;
         }
     }
 
+    /**
+     * Makes the Snake go Up
+     */
     public void goUp() {
         if (!direction.equals(Direction.DOWN)) {
             direction = Direction.UP;
         }
     }
 
+    /**
+     * Makes the Snake go Down
+     */
     public void goDown() {
         if (!direction.equals(Direction.UP)) {
             direction = Direction.DOWN;
